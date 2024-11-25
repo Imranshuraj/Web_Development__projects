7db9d48e1d2ca6140c57a51154fb5a7be0bb62c8
Imranshuraj <imranshuraj@gmail.com>
2024-11-25 16:44:44 +0530

    Practices update

---
+++ js_practice/script.js
@@ -0,0 +1,77 @@
+// let score = false
+
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let isLoggedIn = "sc"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+// let value = 4
+// let negativeValue = -value
+// console.log(negativeValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*3)
+// console.log(2**10)// double ** ka matlab race to the power hota hai
+// console.log(2/2)
+// console.log(2%2)
+
+// let str1 = "Anshu"
+// let str2 = " Gupta"
+// let str3 = " King"
+// let str4 = str1 + str2 + str3
+// console.log(str4);\
+
+// function abcd(a,b,c) {
+//   console.log(a,b,c)
+// }
+// abcd(4,8,454)
+
+// var a =  ["Anshu","Raj","Gupta"];
+// a.push("king");
+
+// var a = ["Anshu","Raj","Gupta"]
+// a.pop()
+
+// var a = ["Anshu","Raj","Gupta"]
+// a.unshift("MR.")
+
+// var a = ["Anshu","Raj","Gupta"]
+// a.shift()
+
+// var a = ["Anshu","Raj","Gupta","King","Ji","Bikram"]
+// a.splice(2,3)
+
+// var anshu = {
+//   rollNo: 21,
+//   fatherName:"Sunil Kumar",
+//   motherName:"Sushma Devi",
+//   year:"1st"
+// }
+// var anshuRollNo = String(anshu.rollNo)
+// console.log(anshuRollNo)
+// console.log(typeof (anshuRollNo))
+
+// const name = "Anshu "
+// const repoCount= 50
+// console.log(name + repocount)//it is old method
+// //or
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount} `);
+
+const gameName = new String('Agk Game')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName);
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+console.log(gameName.toLowerCase())
+console.log(gameName.charAt(4)); //It is used to check the character of a position
+console.log(gameName.indexOf('m'))// it is  used to check the position of a character
